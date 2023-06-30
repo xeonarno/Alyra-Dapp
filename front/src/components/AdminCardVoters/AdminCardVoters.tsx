a671a4e068b4a78ba66d8ddca1602bffdce81969
laugharne <franck@maussand.net>
2023-06-30 14:17:28 +0200

    [DAPP-35] 🔥cleaning code

--- front/src/components/AdminCardVoters/AdminCardVoters.tsx
+++ front/src/components/AdminCardVoters/AdminCardVoters.tsx
@@ -4,7 +4,6 @@ import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
 import { Heading } from '@chakra-ui/react';
 import { Stack, StackDivider } from '@chakra-ui/react';
 import AdminCardVoter from '../AdminCardVoter/AdminCardVoter';
-import { useEffect } from 'react';
 
 import { useVotersContext } from '@/context/voters';
 
