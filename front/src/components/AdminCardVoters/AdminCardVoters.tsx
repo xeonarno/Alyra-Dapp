5ef556daebb37102969fb978230488776416b96d
laugharne <franck@maussand.net>
2023-06-30 14:17:28 +0200

    [DAPP-35] 🔥cleaning code

--- front/src/components/AdminCardVoters/AdminCardVoters.tsx
+++ front/src/components/AdminCardVoters/AdminCardVoters.tsx
@@ -4,7 +4,6 @@ import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
 import { Heading } from '@chakra-ui/react';
 import { Stack, StackDivider } from '@chakra-ui/react';
 import AdminCardVoter from '../AdminCardVoter/AdminCardVoter';
-import { useEffect } from 'react';
 
 import { useVotersContext } from '@/context/voters';
 
@@ -33,9 +32,6 @@ export default function AdminCardVoters() {
 }
 
 /*
-			<AdminCardVoter num={"1"} address={"0x99048293FA822B1C610979122BB987F072a62CcA"} />
-			<AdminCardVoter num={"2"} address={"0x1e3F30A3715D00A91de8dd819ceB75c444CDFD6D"} />
-			<AdminCardVoter num={"3"} address={"0xE82a021e583e2826488611b01CFe870ee051b9ac"} />
 
 0x99048293FA822B1C610979122BB987F072a62CcA
 0x1e3F30A3715D00A91de8dd819ceB75c444CDFD6D
