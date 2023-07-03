ecd3528505c6537c98b0e0ae0e3962e8a1d6d115
Arnaud Palin Sainte Agathe <xeonarno@hotmail.com>
2023-07-03 16:24:53 +0200

    [DAPP-19] feat: :sparkles: add Voter page

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -3,10 +3,10 @@ import Layout from "@/components/Layout/Layout";
 
 import { useAccount } from 'wagmi';
 
-
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useOwnerContext } from "@/context/owner";
 import OwnerPage from "@/components/OwnerPage/OwnerPage";
+import VoterPage from "@/components/VoterPage/VoterPage";
 
 
 export default function Home() {
@@ -14,12 +14,11 @@ export default function Home() {
 
   const { isOwner } = useOwnerContext();
 
-
-
   return (
     <Layout>
       {!isConnected && <ConnectButton />}
       {isConnected && isOwner && <OwnerPage />}
+      {isConnected && !isOwner && <VoterPage />}
     </Layout>
   )
 }
